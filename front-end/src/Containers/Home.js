44b3e6b99ff2b62b7236f7146150f564f449ef0f
Yifan <zyfeleven@gmail.com>
2024-01-26 16:56:49 -0500

    Set up routings

---
+++ front-end/src/Containers/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+function Home(){
+    return(
+        <Container>
+            this is home page
+        </Container>
+    );
+}
+export default Home;
---
+++ front-end/src/Containers/Profile/profile.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Profile(){
+    return(
+        <div>Profile test</div>
+    );
+}
+export default Profile;
