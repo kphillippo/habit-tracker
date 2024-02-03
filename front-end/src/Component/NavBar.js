08482efbf873b9c3fbe6f053dd8fc356ebd8ff00
Yifan <zyfeleven@gmail.com>
2024-02-03 16:18:22 -0500

    update navbar css

--- front-end/src/Component/NavBar.js
+++ front-end/src/Component/NavBar.js
@@ -1,40 +1,76 @@
 import React, { useState } from "react";
-import {Nav, NavItem, NavLink} from "reactstrap"
+import {Nav, NavItem} from "reactstrap"
+import { NavLink } from "react-router-dom";
 
 import "../Css/navbar.css"
 import { Link } from "react-router-dom";
 
 function NavBar() {
-
+    const login = false;
   return (
     <div>
         <Nav className="navbar-top row-12">
-            <NavItem>
-                Lifetrack your path to a better you!
-            </NavItem>              
+            <NavItem className="row-6">
+                <span className="title">Lifetrack</span> 
+                <span>your path to a better you!</span>
+            </NavItem> 
+            
+            <NavItem className="top-right">
+            {login === true && 
+                    <span>Welcome User!</span>
+                }
+
+            {login === false && 
+                    <div className="sign-link-div">
+                    <NavLink
+                        className= "sign-link"
+                        activeClassName = "active"
+                        active
+                        to="/signin"
+                        >
+                        Signin
+                    </NavLink>
+                    <span>/</span>
+                    <NavLink
+                    className= "sign-link"
+                    activeClassName = "active"
+                    active
+                    to="/signup"
+                    >
+                    Signup
+                    </NavLink>
+                    </div>
+
+                }     
+            </NavItem>      
         </Nav>
         <Nav className="navbar-bot row-12">
+
             <NavItem>
                 <NavLink
+                    className = "nav-link"
+                    activeClassName = "active"
                     active
-                    href="/home"
-                    as={Link}
+                    to="/home"
                 >
                 Home
                 </NavLink>
             </NavItem>
 
             <NavItem>
-                <NavLink href="/dailies" 
-                as={Link}>
+                <NavLink 
+                className = "nav-link"
+                activeClassName = "active"
+                to="/dailies" 
+                >
                 Dailies
                 </NavLink>
             </NavItem>
 
             <NavItem>
                 <NavLink
-                as={Link}
-                href="/journal"
+                className = "nav-link"
+                to="/journal"
                 >
                 Journal
                 </NavLink>
@@ -42,8 +78,8 @@ function NavBar() {
 
             <NavItem>
                 <NavLink
-                as={Link}
-                href="/challenges"
+                className = "nav-link"
+                to="/challenges"
                 >
                 Challenges
                 </NavLink>
@@ -51,8 +87,8 @@ function NavBar() {
 
             <NavItem>
                 <NavLink
-                as={Link}
-                href="/leaderboard"
+                className = "nav-link"
+                to="/leaderboard"
                 >
                 My Leaderboard
                 </NavLink>
@@ -60,8 +96,8 @@ function NavBar() {
 
             <NavItem>
                 <NavLink
-                as={Link}
-                href="/stats"
+                className = "nav-link"
+                to="/stats"
                 >
                 My Stats
                 </NavLink>
@@ -69,8 +105,8 @@ function NavBar() {
 
             <NavItem>
                 <NavLink
-                as={Link}
-                href="/help"
+                className = "nav-link"
+                to="/help"
                 >
                 Help
                 </NavLink>
