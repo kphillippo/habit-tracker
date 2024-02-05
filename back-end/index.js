cb92cb6bbeefd8a2e6ecd4c9f0a2481178744c2d
Robert Pierik <robbiep1@hotmail.com>
2024-02-05 17:42:05 -0500

    utilize env file for collection of secrets like dev user and password when connecting to database

--- back-end/index.js
+++ back-end/index.js
@@ -4,7 +4,7 @@ const app = express();
 const env = process.env.NODE_ENV || "development";
 const config = require("./config")[env];
 const mongoose = require("mongoose")
-const mongoDB = "mongodb+srv://user:pass@cluster.rbzvfkr.mongodb.net/"
+const mongoDB = "mongodb+srv://"+process.env.USERNAME+":"+process.env.PASSWORD+"@cluster.rbzvfkr.mongodb.net/";
 
 const PORT = process.env.PORT ||8081;
 
