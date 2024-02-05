f909bfab65f11fd1761afc6dc0453a537604dde6
Robert Pierik <robbiep1@hotmail.com>
2024-02-05 17:29:09 -0500

    Add config and include mongoose for db connection

---
+++ back-end/config.js
@@ -0,0 +1,17 @@
+var config = {
+    production: {
+        //url to be used in link generation
+        url: '',
+        //mongodb connection settings
+        database: {
+            host: 'cluster.rbzvfkr.mongodb.net',
+            port: '27017',
+            db:   'Habit_Tracker'
+        },
+        //server details
+        server: {
+            host:   '127.0.0.1',
+            port:   '3421'
+        }
+    }
+};
--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+const mongoDB = "mongodb+srv://user:pass@cluster.rbzvfkr.mongodb.net/"
 
 const PORT = process.env.PORT ||8081;
 
