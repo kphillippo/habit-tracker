7193a4654905737c97a3cb772552f14345007203
LysaHannes <lysa200125@gmail.com>
2024-02-07 16:23:28 -0500

    tried setting up back end

---
+++ back-end/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    age:{
+        type: Number,
+    },
+    username:{
+        type: String,
+    },
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+module.exports = UserModel;
