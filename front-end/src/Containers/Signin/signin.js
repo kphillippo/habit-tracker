8c28b055fe39b38ad7a7c5032d8fa0e6986a4e66
Yifan <zyfeleven@gmail.com>
2024-02-12 22:29:21 -0500

    update mock data

--- front-end/src/Containers/Signin/signin.js
+++ front-end/src/Containers/Signin/signin.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import { NavLink } from "react-router-dom";
 import "../../Css/signin.css"
+import { FaRegUserCircle, FaLock } from "react-icons/fa";
 
 function Signin(){
 
@@ -19,7 +20,31 @@ function Signin(){
         <div className="main-container">
             <span className="lg-font">Welcome to HabbitConnect! </span> <br></br>
             <span>"I an a random quote every day to give you motivation." -Person McPerson</span>
-            <Form className="log-in-form" onSubmit={handleSubmit}>
+            <div className={'wrapper'}>
+                <form action={""} >
+                    <h1>Login</h1>
+                    <div className={'inputBox'}>
+                        <FaRegUserCircle className="icon"/> Username: <input type={"text"} placeholder={'  Username'} required/>
+
+                    </div>
+                    <div className={'inputBox'}>
+                        <FaLock className="icon"/>     Password: <input type={"password"} placeholder={'  Password'} required/>
+
+                    </div>
+
+                    <button type={"submit"}>Login</button>
+
+                    <div className={'register-link'}>
+                        <p>Don't have an account? <a href={"#"}>Sign up </a></p>
+                    </div>
+
+                    <div className='remember-forget'>
+                        <label><input type="checkbox"/>Remember Me</label>
+                        <a href="#"> Forgot password</a>
+                    </div>
+                </form>
+            </div>
+            {/* <Form className="log-in-form" onSubmit={handleSubmit}>
                 <div className="form-content">
                 <span className="lg-font">Log in</span>
                 <FormGroup row className="formgroup">
@@ -50,7 +75,7 @@ function Signin(){
                 <span>Don't have an account? </span> <NavLink to="/signup"> Sign up </NavLink> <br></br>
                 <NavLink to="/password-recover">Forgot password?</NavLink>
                 </div>
-            </Form>
+            </Form> */}
         </div>
     );
 }
