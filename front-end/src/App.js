caeee168b0e07918b8727ca43e5acc1d7edd751a
zyfeleven <zyfeleven@gmail.com>
2024-02-17 11:54:26 -0500

    remove footer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -40,7 +40,6 @@ function App() {
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-        <Footer />
       </div>
     </Router>
   );
