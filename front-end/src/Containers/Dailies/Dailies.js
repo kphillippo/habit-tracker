56293b7d3dd0881f4263f7accb03bcee2fb4e17a
YoungMaxwell <mwymaxwell@gmail.com>
2024-03-01 20:50:23 -0500

    Adds dailies page

--- front-end/src/Containers/Dailies/Dailies.js
+++ front-end/src/Containers/Dailies/Dailies.js
@@ -1,8 +1,45 @@
 import React from "react";
+import "./dailies.css";
 
 function Dailies(){
-    return(
-        <div className="main-container">dailies test</div>
+  return (
+  <body>
+    <div class="dailies-page">
+        <div class="component-5-child1"></div>
+
+      <div class="dailies-page-child"></div>
+      <div class="dailies-page-item"></div>
+      <div class="my-habits1">My Habits</div>
+      <div class="to-do-list1">To Do List</div>
+
+      <div class="january1">January</div>
+      <div class="dailies-page-inner"></div>
+      <button class="button3">29</button>
+      <div class="calendar-1-icon"></div>
+      <div class="dailies-page-child1"></div>
+      <div class="dailies-page-child2"></div>
+      <div class="dailies-page-child3"></div>
+      <div class="dailies-page-child4"></div>
+      <div class="dailies-page-child17"></div>
+      <div class="habit1">Habit</div>
+      <div class="streak1">Streak</div>
+      <div class="goal1">Goal</div>
+
+      <button class="cog-1"></button>
+      <div class="dailies-page-child24"></div>
+      <input
+        class="type-new-to1"
+        placeholder="Type new to do item here"
+        type="text"
+      />
+
+      <button class="button4">+</button>
+
+      <button class="button5">^</button>
+      <button class="button6">^</button>
+    </div>
+  </body>
     );
-}
+ };
 export default Dailies;
+
