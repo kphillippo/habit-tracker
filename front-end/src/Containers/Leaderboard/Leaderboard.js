710eda036a4bcc47a264214450891c099bf09dfe
Yifan <zyfeleven@gmail.com>
2024-03-14 02:16:10 +0800

    add Guest Feature to Challenges, Journal, Leaderboard and Stats page: if the user is not signed in then jump to sign-in page

--- front-end/src/Containers/Leaderboard/Leaderboard.js
+++ front-end/src/Containers/Leaderboard/Leaderboard.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Leaderboard(){
+function Leaderboard(props){
+
+    let navigate = useNavigate()
+
+    function isSignIn(){
+        if(!props.user.userToken){
+            navigate("/signin")
+        }
+    }
+
+    useEffect(() => {
+        isSignIn();
+    })
+    
     return(
         <div className="main-container">Leaderboard test</div>
     );
--- front-end/src/Containers/Stats/Stats.js
+++ front-end/src/Containers/Stats/Stats.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Stats(){
+function Stats(props){
+
+    let navigate = useNavigate()
+
+    function isSignIn(){
+        if(!props.user.userToken){
+            navigate("/signin")
+        }
+    }
+
+    useEffect(() => {
+        isSignIn();
+    })
+
     return(
         <div className="main-container">stats test</div>
     );
