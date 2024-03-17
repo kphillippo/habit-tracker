7526c3782619818fe91fcdf05f2ecc16b1fbaab8
LysaHannes <lysa200125@gmail.com>
2024-03-17 03:02:58 -0400

    finished testing for friends API

--- back-end/controllers/FriendsController.js
+++ back-end/controllers/FriendsController.js
@@ -6,11 +6,8 @@ const sendFriendRequest = async (req, res) => {
     const {User, FriendsWith} = req.body
   
     try{
-        //sees is the friend exists and returns an id
-        const theFriend = await UserModel.getUserId(FriendsWith);
-        
         //trys to send friend request
-        const request = await Friend.sendFriendRequest(User, theFriend)
+        const request = await Friend.sendFriendRequest(User, FriendsWith)
     
         res.status(200).json(request)
     }catch(error){
@@ -21,29 +18,20 @@ const sendFriendRequest = async (req, res) => {
 //returns friends list
 const returnFriendsList = async (req, res) => {
     const {User} = req.body
-  
-    try{
-        //returns friends list
-        const request = await Friend.findFriends(User);
-    
-        res.status(200).json(request)
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
+
+    //returns friends list
+    const request = await Friend.findFriends(User);
+
+    res.status(200).json(request)
 }
 
 //returns friend requests
 const returnFriendRequests = async (req, res) => {
     const {User} = req.body
-  
-    try{
-        //returns friends list
-        const request = await Friend.findFriendRequests(User);
-    
-        res.status(200).json(request)
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
+    //returns friends list
+    const request = await Friend.findFriendRequests(User);
+
+    res.status(200).json(request)
 }
 
 //accepts a friend request
--- back-end/tests/friends.test.js
+++ back-end/tests/friends.test.js
@@ -23,8 +23,8 @@ describe('Freinds API', () => {
 
         //test data
         const testData = {
-        User: '65ee641ca982142253c9e07c',
-        FriendsWith: 'rational'
+        User: '65f690238d4313103042bbe7',
+        FriendsWith: '65f6902c8d4313103042bbeb'
         };
 
         //send friend request
@@ -36,32 +36,13 @@ describe('Freinds API', () => {
         expect(response.status).toBe(200); // Assuming successful user creation returns status 200
     });
 
-    //Fail to send a friend request, already friends
-    test('Fail to Send Friend Request, already friends', async () => {
-
-        //test data
-        const testData = {
-        User: '65f4aaf187bd736b657fe9ea',
-        FriendsWith: 'zyf'
-        };
-
-        //send friend request
-        const response = await request(app)
-        .post('/api/friends/sendFriendRequest')
-        .send(testData)
-        .set('Accept', 'application/json');
-
-        expect(response.status).toBe(400); // Assuming successful user creation returns status 400
-        expect(response.body.error).toBe('You are already friends with this user!'); //expected error message
-    });
-
     //Fail to send a friend request, already sent a friend request to the user
     test('Fail to Send Friend Request, already sent a friend request to this user', async () => {
 
         //test data
         const testData = {
-        User: '65ee641ca982142253c9e07c',
-        FriendsWith: 'rational'
+        User: '65f690238d4313103042bbe7',
+        FriendsWith: '65f6902c8d4313103042bbeb'
         };
 
         //send friend request
@@ -79,8 +60,8 @@ describe('Freinds API', () => {
 
         //test data
         const testData = {
-        User: '65ee645da982142253c9e080',
-        FriendsWith: 'cristalKittyyyyyy'
+        User: '65f6902c8d4313103042bbeb',
+        FriendsWith: '65f690238d4313103042bbe7'
         };
 
         //send friend request
@@ -93,13 +74,50 @@ describe('Freinds API', () => {
         expect(response.body.error).toBe('You have a friend request from this user already!'); //expected error message
     });
 
+    //Accept Friend Request
+    test('Accept Friend Request', async () => {
+
+        //test data
+        const testData = {
+            User: '65f6902c8d4313103042bbeb',
+            FriendsWith: '65f690238d4313103042bbe7'
+        };
+
+        //send friend request
+        const response = await request(app)
+        .post('/api/friends/acceptFriendRequest')
+        .send(testData)
+        .set('Accept', 'application/json');
+
+        expect(response.status).toBe(200); // Assuming successful user creation returns status 200
+    });
+
+    //Fail to send a friend request, already friends
+    test('Fail to Send Friend Request, already friends', async () => {
+
+        //test data
+        const testData = {
+            User: '65f6902c8d4313103042bbeb',
+            FriendsWith: '65f690238d4313103042bbe7'
+        };
+
+        //send friend request
+        const response = await request(app)
+        .post('/api/friends/sendFriendRequest')
+        .send(testData)
+        .set('Accept', 'application/json');
+
+        expect(response.status).toBe(400); // Assuming successful user creation returns status 400
+        expect(response.body.error).toBe('You are already friends with this user!'); //expected error message
+    });
+
     //Fail to send a friend request, cannot friend yourself
     test('Fail to Send Friend Request, cannot friend yourself', async () => {
 
         //test data
         const testData = {
-        User: '65ee641ca982142253c9e07c',
-        FriendsWith: 'cristalKittyyyyyy'
+            User: '65f6902c8d4313103042bbeb',
+            FriendsWith: '65f6902c8d4313103042bbeb'
         };
 
         //send friend request
@@ -111,4 +129,88 @@ describe('Freinds API', () => {
         expect(response.status).toBe(400); // Assuming successful user creation returns status 400
         expect(response.body.error).toBe('You cannot friend yourself!'); //expected error message
     });
+
+    //Delete Freind
+    test('Delete Friend', async () => {
+
+        //test data
+        const testData = {
+            User: '65f6902c8d4313103042bbeb',
+            FriendsWith: '65f690238d4313103042bbe7'
+        };
+
+        //send friend request
+        const response = await request(app)
+        .post('/api/friends/deleteFriend')
+        .send(testData)
+        .set('Accept', 'application/json');
+
+        expect(response.status).toBe(200); // Assuming successful user creation returns status 200
+    });
+
+    //Decline Freind Request (creates a friend request first)
+    test('Decline Friend Request', async () => {
+
+        //test data
+        const testData = {
+            User: '65f690238d4313103042bbe7',
+            FriendsWith: '65f6902c8d4313103042bbeb'
+        };
+    
+        //send friend request
+        const response = await request(app)
+        .post('/api/friends/sendFriendRequest')
+        .send(testData)
+        .set('Accept', 'application/json');
+
+        //test data2
+        const testData2 = {
+            User: '65f6902c8d4313103042bbeb',
+            FriendsWith: '65f690238d4313103042bbe7'
+        };
+
+        //decline friend request
+        const response2 = await request(app)
+        .post('/api/friends/declineFriendRequest')
+        .send(testData2)
+        .set('Accept', 'application/json');
+
+        expect(response2.status).toBe(200); // Assuming successful user creation returns status 200
+    });
+
+    //Return a list of all friend requests
+    test('Return a list of all friend requests', async () => {
+
+        //test data
+        const testData = {
+            User: '65f690238d4313103042bbe7'
+        };
+    
+        //send friend request
+        const response = await request(app)
+        .post('/api/friends/returnFriendRequests')
+        .send(testData)
+        .set('Accept', 'application/json');
+
+        expect(response.status).toBe(200); // Assuming successful user creation returns status 200
+    });
+
+    //Return a list of all friends
+    test('Return a list of all friends', async () => {
+
+        //test data
+        const testData = {
+            User: '65f690238d4313103042bbe7'
+        };
+    
+        //send friend request
+        const response = await request(app)
+        .post('/api/friends/returnFriendsList')
+        .send(testData)
+        .set('Accept', 'application/json');
+
+        expect(response.status).toBe(200); // Assuming successful user creation returns status 200
+    });
+
+    //return freinds list
 });
