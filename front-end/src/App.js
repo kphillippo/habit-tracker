e069056d14533707134184f1ee9f9b3e3e810ada
ed18xy <ed18xy@brocku.ca>
2024-03-20 15:33:34 -0400

    user page updates

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -68,7 +68,7 @@ function App() {
           <Route path="/leaderboard" element={<Leaderboard user={{userName, userToken, userStreak}} toast={toast}/>} />
           <Route path="/stats" element={<Stats user={{userName, userToken, userStreak}} toast={toast}/>} />
           <Route path="/help" element={<Help />} />
-          <Route path="/profile" element={<Profile data={{userName, userToken}} toast={toast}/>} />
+          <Route path="/profile" element={<Profile userinfoUpdated={() => setisUpdated(true)} data={{userName, userToken}} toast={toast}/>} />
           <Route path="/signin" element={<Signin isSignedin={() => setisUpdated(true)} toast={toast}/>} />
           <Route path="/signup" element={<Signup isSignedin={() => setisUpdated(true)} toast={toast}/>} />
           <Route path="/" element={<Navigate replace to="/home" />} />
--- front-end/src/Containers/Profile/ViewFriendPopup.js
+++ front-end/src/Containers/Profile/ViewFriendPopup.js
@@ -1,19 +1,23 @@
-import "../../Css/ViewFriendPopup.css";
-import React, { useState, useEffect } from "react";
-import { apiRequest } from "../../utils/reqTool";
+import React from "react";
+import "../../Css/userProfilePopups.css";
 
+/**
+ * 
+ * @param {*} parametersForPopup - function to close popup, friend to view
+ * @returns popup to see friend profile information
+ */
 function ViewFriendPopup({ onClose, friend }) {
-  
   return (
-    <div className="bg_blurV">
-      <div className="mainFrameV">
-        <div className="innerFrameV">
+    <div className="bg_blur">
+      <div className="mainFrame">
+        <div className="innerFrame">
           <center><div className="Title">
             View Friend Profile<br></br>
-           ID:  {friend}
+            {//display friend info
+            friend.username}
           </div>
           </center>
-          <button className="popup_closeA" onClick={onClose}>X</button>
+          <button className="popup_close" onClick={onClose}>X</button>
         </div>
       </div>
     </div>
