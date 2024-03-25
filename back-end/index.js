c8c8a226a94b145bbd948095e908030c3afdadb9
LysaHannes <lysa200125@gmail.com>
2024-03-24 23:29:28 -0400

    Added API to send an email

--- back-end/index.js
+++ back-end/index.js
@@ -20,6 +20,7 @@ const habitRoutes = require('./routes/HabitRoutes.js');
 const todoRoutes = require('./routes/TodoRoutes.js');
 const friendsRoutes = require('./routes/FriendsRoutes.js');
 const habitCheckInRoutes = require('./routes/HabitCheckInRoutes.js');
+const verificationRoute = require('./routes/VerificationRoutes.js');
 
 /*
 //variables for image
@@ -39,6 +40,7 @@ app.use('/api/habit', habitRoutes)
 app.use('/api/todo', todoRoutes)
 app.use('/api/friends', friendsRoutes)
 app.use('/api/habitCheckIn', habitCheckInRoutes)
+app.use('/api/verificartion', verificationRoute)
 
 mongoose.connect(serverLink);
 
