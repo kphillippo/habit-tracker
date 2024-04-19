2b50e4a44e20d7bac9c2ffbcf52171d531483f73
Robert Pierik <robbiep1@hotmail.com>
2024-04-19 18:05:51 -0400

    ToDoCheckIn router added

--- back-end/routes/StatisticsRoutes.js
+++ back-end/routes/StatisticsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 
-const { habitsCompletedLast30Days } = require('../controllers/StatisticsController');
+const { habitsCompletedLast30Days, timesHabitCompletedLast30Days } = require('../controllers/StatisticsController');
 
 const router = express.Router()
 router.get('/habitsCompletedLast30Days', habitsCompletedLast30Days)
+router.get('/timesHabitCompletedLast30Days', timesHabitCompletedLast30Days)
 
 module.exports = router
--- back-end/routes/ToDoCheckInRoutes.js
+++ back-end/routes/ToDoCheckInRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 
 //controller functions
-const { getCheckIns, updateCheckIn } = require('../controllers/ToDoCheckInController')
+const { getCheckIns, updateCheckIn, getTodayStatus } = require('../controllers/ToDoCheckInController')
 
 const router = express.Router()
 
 //any routes go here
 router.get('/getCheckIns', getCheckIns);
 router.post('/updateCheckIn', updateCheckIn)
+router.get('/getTodayStatus', getTodayStatus)
 
 
 
